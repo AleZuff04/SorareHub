2294bd4c22e9500ed1d35d456ea3fac9ad73e3ba
Alessandro Zuffoli <alessandro.zuffoli@edu.unife.it>
2026-08-12 11:17:23 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
   plugins: [
+    TanStackRouterVite(),
     react(),
-    tsconfigPaths(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
   server: {
     port: 5173,
   },
