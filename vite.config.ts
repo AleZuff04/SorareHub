2080ffac157eb26f7792a093758ec39a55b5cb47
Alessandro Zuffoli <alessandro.zuffoli@edu.unife.it>
2026-08-12 11:30:00 +0200

    Add tsconfig paths plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
     react(),
+    tsconfigPaths(),
   ],
-  resolve: {
-    tsconfigPaths: true,
-  },
   server: {
     port: 5173,
   },
